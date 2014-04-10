7a044b4450f100eedc697cf8354569aefc663d1b
jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
2014-04-10 11:26:43 +0100

    Upload functionality done

---
+++ src/mainPackage/TournamentFile.java
@@ -0,0 +1,49 @@
+package mainPackage;
+
+import java.io.File;
+
+/**
+ *
+ * @author jay-to-the-dee <jay-to-the-dee@users.noreply.github.com>
+ */
+public class TournamentFile
+{
+    private final File brainFile;
+    
+    private int wins;
+    private int draws;
+    private int loses;
+
+    public TournamentFile(File brainFile)
+    {
+        this.brainFile = brainFile;
+    }
+
+    public File getBrainFile()
+    {
+        return brainFile;
+    }
+
+    @Override
+    public String toString()
+    {
+        return brainFile.getName();
+    }
+    
+    public int getWins()
+    {
+        return wins;
+    }
+
+    public int getDraws()
+    {
+        return draws;
+    }
+
+    public int getLoses()
+    {
+        return loses;
+    }
+    
+    
+}
